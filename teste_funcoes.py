474b01055b7caa3e6c0941063218f30bc88ea97f
antuniassi <marinaantuniassi@outlook.com>
2026-03-13 11:01:13 -0300

    import

--- teste_funcoes.py
+++ teste_funcoes.py
@@ -1,5 +1,3 @@
-from servidor import *
-
-# def teste_lista_imoveis_todos_atributos():
-#     rota = lista_imoveis_todos_atributos()
-#     assert rota == '/lista_imoveis'
+import pytest
+from unittest.mock import patch, MagicMock
+from servidor import app, connect_db
