fdf07faa994838bf7c3332ab875bc7b3ab080ce4
Maria Eduarda dos Santos <mariaes3@al.insper.edu.br>
2026-03-13 10:35:35 -0300

    atualizacao

--- teste_funcoes.py
+++ teste_funcoes.py
@@ -3,7 +3,3 @@ from servidor import *
 # def teste_lista_imoveis_todos_atributos():
 #     rota = lista_imoveis_todos_atributos()
 #     assert rota == '/lista_imoveis'
-
-def teste_index():
-    rota = index()
-    assert rota == '/'
