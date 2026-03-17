40ef1616f9fd9393f84a31889f546a3ca78e03f3
Maria Eduarda dos Santos <mariaes3@al.insper.edu.br>
2026-03-17 10:45:12 -0300

    test_delet_imovel

--- test_imoveis.py
+++ test_imoveis.py
@@ -226,6 +226,36 @@ def test_atualizar_imovel_inexistente(mock_connect_db, client):
 
 
 @patch("servidor.connect_db")
+def test_elete_imovel (mock_connect_db, client):
+    """Testa a exclução de um imóvel existente"""
+
+    # Given
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+    mock_conn.cursor.return_value = mock_cursor
+    mock_cursor.rowcount = 1
+    mock_connect_db.return_value = mock_conn
+
+    response = client.put("/imoveis/1", json=dados_imovel_teste)
+
+     # Then
+    assert response.status_code == 200
+    assert response.get_json() == {"mensagem": "Imóvel atualizado com sucesso"}
+
+    mock_cursor.execute.assert_called_once_with(
+        """
+        DELET from imoveis WHERE id=%s
+        """,
+        (
+            1
+        )
+    )
+
+    mock_conn.commit.assert_called_once()
+
+
+
+@patch("servidor.connect_db")
 def test_listar_imoveis_por_tipo(mock_connect_db, client):
     """Testa listar imoveis por tipo"""
 
